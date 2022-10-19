cd3d3fe4bdc848059980c976123e30e34666d726
tested2020@ <smyxbrone@gmail.com>
2022-10-19 11:49:12 +0100

    'add_class_template'

--- klass/models.py
+++ klass/models.py
@@ -28,13 +28,14 @@ class Klass(models.Model):
     )
     name = models.CharField(max_length=100)
     no_of_students = models.IntegerField()
-    subject = models.JSONField(default=_subject_json)
-    teacher = models.ForeignKey("account.User", blank=True, null=True)
+    subject = models.JSONField(default=_subject_json())
+    teacher = models.ForeignKey(
+        "account.User", on_delete=models.CASCADE, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     session = models.CharField(choices=SESSION_CHOICES, max_length=100)
     year = models.DateTimeField()
-    token = models.CharField(max_length=30, default=generate_token)
-    previous_teachers = models.JSONField(default=__previous)
+    # token = models.CharField(max_length=30, default=generate_token)
+    previous_teachers = models.JSONField(default=_previous())
 
     def __str__(self) -> str:
         return self.name
