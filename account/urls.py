138ac2a0ab403d402f2de461bb8c13384deb668d
tested2020@ <smyxbrone@gmail.com>
2022-10-24 09:03:44 +0100

    pip

--- account/urls.py
+++ account/urls.py
@@ -10,13 +10,13 @@ from .views import RegisterView, UserLogin, UserLogout, Home, landing_page, Chec
 
 urlpatterns = [
     path('home', Home.as_view(), name='admin-page'),
-<<<<<<< HEAD
+    # <<<<<<< HEAD
     path('index/', index, name='index'),
     path('index/result_view', result_view, name='result-view'),
     path('', landing_page, name="landing-page"),
-=======
+    # =======
     path('', landing_page, name="home"),
->>>>>>> 4a949e757817f2b32e6667a8cf16864640dbf779
+    # >>>>>>> 4a949e757817f2b32e6667a8cf16864640dbf779
     path('login/', UserLogin.as_view(), name='login'),
     path('logout/', UserLogout.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
